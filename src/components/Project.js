aa48a8274ab9f361432761158f62ef4e0eb93bdd
Tahrin Maruf <tm5682@mun.ca>
2022-01-11 21:48:58 -0330

    project components changed to cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,27 +1,52 @@
 //importing font awesome icons from react-icons
 import { FaTimes } from "react-icons/fa";
 
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import { IconButton, Typography } from "@mui/material";
+import DeleteOutlined from "@mui/icons-material/DeleteOutlined";
+import { Box } from "@mui/system";
+
 const Project = ({ project, onDelete, onToggle }) => {
   return (
-    <div
-      //if current status true then use reminder css class else project class
-      className={`project ${project.favorite ? "reminder" : ""}`}
-      onDoubleClick={() => onToggle(project.id)}
-    >
-      <br />
-      <h3>
-        {project.name}
-        <FaTimes
-          style={{ color: "red", cursor: "pointer" }}
-          onClick={() => onDelete(project.id)}
-        />
-      </h3>
-      <br />
-      <p>
-        {" "}
-        <b>Client Name:</b> {project.clientName}{" "}
-      </p>
-      <br />
+    // <div
+    //   //if current status true then use reminder css class else project class
+    //   className={`project ${project.favorite ? "reminder" : ""}`}
+    //   onDoubleClick={() => onToggle(project.id)}
+    // >
+    //   <br />
+    //   <h3>
+    //     {project.name}
+    //     <FaTimes
+    //       style={{ color: "red", cursor: "pointer" }}
+    //       onClick={() => onDelete(project.id)}
+    //     />
+    //   </h3>
+    //   <br />
+    //   <p>
+    //     {" "}
+    //     <b>Client Name:</b> {project.clientName}{" "}
+    //   </p>
+    //   <br />
+    // </div>
+
+    <div>
+      <Box display="flex" flexDirection="column">
+        <Card>
+          <CardHeader
+            onClick={() => console.log("Toggler")}
+            key={project.id}
+            action={
+              <IconButton onClick={() => onDelete(project.id)}>
+                <DeleteOutlined />
+              </IconButton>
+            }
+            title={project.name}
+            subheader={project.clientName}
+          />
+        </Card>
+      </Box>
     </div>
   );
 };
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,30 @@
 import Project from "./Project";
 
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+
 const ProjectList = ({ projectList, onDelete, onToggle }) => {
-  return projectList.map((project) => (
-    <Project
-      key={project.id}
-      project={project}
-      onDelete={onDelete}
-      onToggle={onToggle}
-    />
-  ));
+  return (
+    // <Project
+    //   key={project.id}
+    //   project={project}
+    //   onDelete={onDelete}
+    //   onToggle={onToggle}
+    // />
+    <Container>
+      <Grid container spacing={3}>
+        {projectList.map((project) => (
+          <Grid item key={project.id} xs={12}>
+            <Project
+              project={project}
+              onDelete={onDelete}
+              onToggle={onToggle}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 };
 
 export default ProjectList;
