90c82502154bcb102879c736d9456682fc5ffe52
Tahrin Maruf <tm5682@mun.ca>
2022-01-24 22:57:11 -0330

    firebase integration for add issues, material ui timepicker with luxon, multiple labels adder

--- src/components/Create.js
+++ src/components/Create.js
@@ -5,7 +5,7 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 /* import { ButtonGroup } from "@mui/material"; */
 
-import { Container, FormControlLabel } from "@mui/material";
+import { Container, Divider, FormControlLabel } from "@mui/material";
 
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
@@ -15,7 +15,7 @@ import { Radio } from "@mui/material";
 
 import { RadioGroup } from "@mui/material";
 
-import { FormControl } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
 
 import { FormLabel } from "@mui/material";
 
@@ -25,6 +25,15 @@ import Grid from "@mui/material/Grid";
 
 import Chip from "@mui/material/Chip";
 import Autocomplete from "@mui/material/Autocomplete";
+
+// date-fns
+//import DateAdapter from "@mui/lab/AdapterDateFns";
+
+//luxon
+import DateAdapter from "@mui/lab/AdapterLuxon";
+
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import MobileDatePicker from "@mui/lab/MobileDatePicker";
 
 import { db } from "../firebase";
 import {
@@ -69,41 +78,68 @@ function Create() {
   //to hold the issue tags
   const [issueTags, setIssueTags] = useState([]);
 
-  const handleSubmit = (e) => {
+  //today's date - we use it as default for project due date if no date is passed
+  const timeElapsed = Date.now();
+  const today = new Date(timeElapsed);
+
+  //to hold due date
+  const [dueDate, setDueDate] = useState(today.toLocaleDateString());
+  //to handle submission of the form
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    //unique id for the issue
+    const newIssueId = cuid();
+
+    //data to upload as new issue
+    const newIssue = {
+      id: newIssueId,
+      name: title,
+      details: details,
+      severity: severity,
+      tags: issueTags || null,
+      dueDate: dueDate.ts || null,
+    };
 
     setTitleError(false);
     setDetailsError(false);
 
-    if (title == "") {
+    if (title === "") {
       setTitleError(true);
     }
 
-    if (details == "") {
+    if (details === "") {
       setDetailsError(true);
     }
 
     if (title && details) {
-      fetch("http://localhost:8000/issues", {
-        method: "POST",
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify({ title, details, severity, issueTags }),
-      }).then(() => navigate("/issues"));
+      await setDoc(doc(db, "issues", newIssueId), newIssue);
+      console.log("Data passed", newIssue);
     }
+
+    //old json-server post code
+    // {
+    //   fetch("http://localhost:8000/issues", {
+    //     method: "POST",
+    //     headers: { "Content-type": "application/json" },
+    //     body: JSON.stringify({ title, details, severity, issueTags }),
+    //   }).then(() => navigate("/issues"));
+    // }
   };
 
   //adds to issue tag state
-  const onIssueTagChange = (event) => {
-    let newIssueTag = event.target.value;
+  const onIssueTagChange = (event, newTags) => {
+    //let newIssueTag = event.target.value;
 
-    console.log("NEW ISSUE TAG", newIssueTag);
+    // console.log(newTags);
 
-    setIssueTags((params) => [...params, newIssueTag]);
+    setIssueTags(newTags);
   };
 
-  const foo = (option) => {
-    console.log(option);
-    setIssueTags((params) => [...params, option]);
+  //to handle due date change
+  const dueDateChange = (dueDate) => {
+    console.log(dueDate);
+    setDueDate(dueDate);
   };
 
   return (
@@ -178,6 +214,7 @@ function Create() {
               {/* MUI Tags */}
 
               <Autocomplete
+                onChange={onIssueTagChange}
                 multiple
                 id="tags-filled"
                 options={tagOptions}
@@ -190,7 +227,6 @@ function Create() {
                       <Chip
                         variant="outlined"
                         label={option}
-                        onChange={onIssueTagChange}
                         {...getTagProps({ index })}
                       />
                     );
@@ -204,8 +240,20 @@ function Create() {
                     placeholder="Issue Tags"
                   />
                 )}
-                onChange={onIssueTagChange}
               />
+            </FormControl>
+
+            {/* date picker */}
+            <FormControl sx={{ mt: 2.5 }}>
+              <LocalizationProvider dateAdapter={DateAdapter}>
+                <MobileDatePicker
+                  label="Issue Due Date"
+                  inputFormat="MM/dd/yyyy"
+                  value={dueDate}
+                  onChange={dueDateChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
             </FormControl>
           </Grid>
         </Grid>
@@ -228,7 +276,75 @@ function Create() {
   );
 }
 
-const tagOptions = ["galley", "abc"];
+//for now keeping the tag list here
+const tagOptions = [
+  "galley",
+  "cleaning",
+  "sea water cooling pipework",
+  "starboard generator",
+  "Micro-commander",
+  "UPS",
+  "Generator",
+  "Main Engine",
+  "Propulsion",
+  "steering",
+  "crane",
+  "bow thruster",
+  "haul",
+  "rudder",
+  "stern frame",
+  "Tank soundings",
+  "Fuel tank #3 PORT",
+  "Engine Room",
+  "Dishwasher",
+  "Public Address System",
+  "windlass",
+  "Navigation Lights",
+  "Fuses",
+  "A-Frame",
+  "hydraulic",
+  "Radar",
+  "Magnetron",
+  "Wiper Seal",
+  "hydraulic cylinder",
+  "telephone",
+  "convection oven",
+  "dry store",
+  "SKF Coupling",
+  "Intermediate shaft",
+  "docking",
+  "Assessment",
+  "Repair",
+  "HQ",
+  "Hull",
+  "Silva",
+  "Labour",
+  "Parts",
+  "Piping",
+  "electronic",
+  "Electrical",
+  "KS",
+  "Mechanical",
+  "operations",
+  "Trim Tabs",
+  "Certificate",
+  "Inspection",
+  "harness",
+  "lanyard",
+  "Striker",
+  "Survey",
+  "Fire Detection",
+  "Fire safety",
+  "Life Saving",
+  "Safety",
+  "Lift",
+  "LIfting Gear",
+  "Davits",
+  "hauler",
+  "davit",
+  "Rigging",
+  "Report",
+];
 
 export default Create;
 
