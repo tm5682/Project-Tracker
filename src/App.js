2ef13a56c05194fd826db995e75a2e4f99282efd
Tahrin Maruf <tm5682@mun.ca>
2022-02-02 03:59:48 -0330

    minor changes to table

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
             path="/"
             element={
               <Container>
-                <div></div>
+                <ProjectPage />
               </Container>
             }
           />
